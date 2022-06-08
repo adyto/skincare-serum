5b697f925b3b4f43f4260e91e0b2e70655cb7492
adyto <adiyulianto61@gmail.com>
2022-06-08 19:14:03 +0700

    refresh error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
+import Image from 'next/image';
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <>
+    <section className='mainBanner'>
       <div className='flex flex-col items-center justify-center w-full h-screen space-y-8 lg-flex-row'>
         <h1 className='text-3xl'> Serum Skin Care</h1>
         <div className='relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4 '>
@@ -13,17 +12,15 @@ const Home: NextPage = () => {
         </div>
         <div className='relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4 '>
           <Image
-            src='/img/serum-2.png' objectFit='cover' alt='Picture of Serum' layout='fill' priority
+            src="/img/serum-2.png" objectFit="cover" alt='Picture of Serum' layout='fill' priority
           />
         </div>
         <div className='relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4 '>
           <Image
-            src='/img/serum-3.png' objectFit='cover' alt='Picture of Serum' layout='fill' priority
+            src="/img/serum-3.png" objectFit="cover" alt="Picture of Serum" layout="fill" priority
           />
         </div>
       </div>
-    </>
+    </section>
   )
 }
-
-export default Home
